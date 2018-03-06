6ffb8ec366befb34862d2280015164868de503d6
Pokitto <pokitto@pokitto.com>
2018-03-07 00:54:55 +0200

    Typo on line 1432 / PokittoDisplay.cpp

--- Pokitto/POKITTO_CORE/PokittoDisplay.cpp
+++ Pokitto/POKITTO_CORE/PokittoDisplay.cpp
@@ -1429,7 +1429,7 @@ void Display::drawBitmapData(int16_t x, int16_t y, int16_t w, int16_t h, const u
 	
 	
     /** 4bpp fast version */
-    if (m_colordepth<4) {
+    if (m_colordepth==4) {
 
     /** 4bpp fast version */
 	int16_t scrx,scry,xclip,xjump,scrxjump;
