56564ace0abbb720ef7fd6428eee6c66ef8937ab
haviital <hannu.j.viitala@gmail.com>
2018-05-08 00:24:04 +0300

    Added needed files to the EmBitz Tracker-project.

--- Examples/Tracker/My_settings.h
+++ Examples/Tracker/My_settings.h
@@ -18,7 +18,6 @@
 #define PROJ_STREAMING_MUSIC 0
 #define PROJ_SOUND_BUFFERED  0 // use buffered IRQ
 #define PROJ_STREAM_TO_DAC   0 // synth to DAC
-//#define NOPETITFATFS
 #define PROJ_AUD_FREQ 44100 //22050 //11025//22050
 #define PROJ_FPS 20
 //#define PROJ_SHOW_FPS_COUNTER
