db045ee5380283902ca8787e5302f89d6e034fb0
Jonne Valola <jonnev@gmail.com>
2018-06-10 04:34:54 +0300

    saveblocks system being created

--- Examples/HelloWorld/main.cpp
+++ Examples/HelloWorld/main.cpp
@@ -1,18 +1,35 @@
 #include "Pokitto.h"
+#include "PokittoSaveblock.h"
 
 Pokitto::Core mygame;
 
+class saveb : public Pokitto::Saveblock {
+    int val=0;
+    int goo=35;
+};
+
+saveb mysave;
 
 int main () {
     mygame.begin();
+    mysave.formatKeyTable();
+    mysave.begin("TESTTEST");
     int a = random(100);
+    char answer[9];
+
     while (mygame.isRunning()) {
 
         if (mygame.update()) {
             mygame.display.println(a);
             mygame.display.println((int)Pokitto::Battery::level);
-            mygame.display.print("Hello World!");
-
+            mygame.display.println("Hello World!");
+            mysave.getKeyAt(0, answer);
+            mygame.display.println(answer);
+            mysave.getKeyAt(1, answer);
+            mygame.display.println(answer);
+            mysave.getKeyAt(2, answer);
+            mygame.display.println(answer);
+            mygame.display.println(sizeof(mysave));
         }
         }
 
