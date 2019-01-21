c18e76283b1b24cc9cd37e784550df48a55aea01
Jonne <pokitto@pokitto.com>
2019-01-21 02:14:22 +0200

    added DISABLEAVRMIN to HelloWorld target

--- Examples/HelloWorld/My_settings.h
+++ Examples/HelloWorld/My_settings.h
@@ -14,5 +14,6 @@
 
 #define PROJ_HIRES 0            //1 = high resolution (220x176) , 0 = low resolution fast mode (110x88)
 #define PROJ_ENABLE_SOUND 0     // 0 = all sound functions disabled
+#define DISABLEAVRMIN
 
 #endif
