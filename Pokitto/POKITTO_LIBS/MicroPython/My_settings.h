c95319e3f072bda86321220b7fb9c0af9bc0fba9
haviital <hannu.j.viitala@gmail.com>
2019-04-03 23:57:18 +0300

    Cleanup code

--- Pokitto/POKITTO_LIBS/MicroPython/My_settings.h
+++ Pokitto/POKITTO_LIBS/MicroPython/My_settings.h
@@ -18,24 +18,12 @@
 
 // Sound
 #define PROJ_STREAMING_MUSIC 1
-//#define PROJ_ENABLE_SYNTH 0
 #define PROJ_ENABLE_SOUND       1       // 0 = all sound functions disabled
 #define PROJ_AUD_FREQ           8000       // 44100 //22050 //11025
-//#define PROJ_STREAM_TO_DAC      1      // 1 use DAC for stream, 0 = use PWM for stream
-//#define PROJ_USE_PWM            1      // 1 use PWM, 0 do not use PWM
-//#define PROJ_GBSOUND            0       // 1 = use Gamebuino-compatible sound interrupt (choose this or the one below)
-
 
 #ifndef POK_SIM
 #define PROJ_HIGH_RAM HIGH_RAM_MUSIC
 #endif
-
-//#define PROJ_ENABLE_SOUND 1     // 0 = all sound functions disabled
-//#define PROJ_STREAMING_MUSIC 1
-//#define PROJ_SDFS_STREAMING
-//#define PROJ_AUD_FREQ 8000
-
-
 
 #define PROJ_SHOW_FPS_COUNTER
 #define PROJ_FPS 200
