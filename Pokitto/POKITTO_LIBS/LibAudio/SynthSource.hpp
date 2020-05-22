baa5257ac00823dd32a666670cb9caf7262fbc31
FManga <fmanga@localhost.localdomain>
2020-05-22 20:32:21 +0100

    Cleanup Synth API

--- Pokitto/POKITTO_LIBS/LibAudio/SynthSource.hpp
+++ Pokitto/POKITTO_LIBS/LibAudio/SynthSource.hpp
@@ -58,7 +58,78 @@ namespace Audio {
     };
 
     template<u32 channel = 0>
-    static void playOSC(){
+    inline void play(u32 oscId, const OSC& osc){
         SynthSource::playOSC<channel>();
+        OSC *oscillators[] = {&osc1, &osc2, &osc3, &osc3};
+        *oscillators[oscId&0x3] = osc;
     }
+
+    struct Note {
+        constexpr Note(u32 notenumber) : _notenumber(notenumber){}
+
+        template<u32 channel = 0>
+        void play(u32 oscId = 0) const {
+            Audio::play<channel>(oscId, *this);
+        }
+
+        uint8_t _wave = 1;
+        constexpr Note& wave(uint8_t v){ _wave = v; return *this; }
+
+        uint8_t _loop = 0;
+        constexpr Note& loop(uint8_t v){ _loop = v; return *this; }
+
+        uint8_t _echo = 0;
+        constexpr Note& echo(uint8_t v){ _echo = v; return *this; }
+
+        uint8_t _adsr = 0;
+        constexpr Note& adsr(uint8_t v){ _adsr = v; return *this; }
+
+        uint8_t _notenumber = 25;
+        constexpr Note& noteNumber(uint8_t v){ _notenumber = v; return *this; }
+
+        uint16_t _volume = 127;
+        constexpr Note& volume(uint16_t v){ _volume = v; return *this; }
+
+        uint16_t _attack = 0;
+        constexpr Note& attack(uint16_t v){ _attack = v; return *this; }
+
+        uint16_t _decay = 0;
+        constexpr Note& decay(uint16_t v){ _decay = v; return *this; }
+
+        uint16_t _sustain = 0;
+        constexpr Note& sustain(uint16_t v){ _sustain = v; return *this; }
+
+        uint16_t _release = 0;
+        constexpr Note& release(uint16_t v){ _release = v; return *this; }
+
+        int16_t _maxbend = 0;
+        constexpr Note& maxbend(int16_t v){ _maxbend = v; return *this; }
+
+        int16_t _bendrate = 0;
+        constexpr Note& bendrate(int16_t v){ _bendrate = v; return *this; }
+
+        uint8_t _arpmode = 0;
+        constexpr Note& arpmode(uint8_t v){ _arpmode = v; return *this; }
+
+        uint8_t _overdrive = 0;
+        constexpr Note& overdrive(uint8_t v){ _overdrive = v; return *this; }
+
+        uint8_t _kick = 0;
+        constexpr Note& kick(uint8_t v){ _kick = v; return *this; }
+
+        uint32_t _duration = ~0;
+        constexpr Note& duration(uint32_t v){ _duration = v; return *this; }
+
+        operator OSC () const {
+            OSC osc;
+            setOSC(&osc, 1,
+                   _wave,  _loop,  _echo,  _adsr,
+                   _notenumber,  _volume,
+                   _attack,  _decay,  _sustain,  _release,
+                   _maxbend,  _bendrate,  _arpmode,  _overdrive,  _kick
+                );
+            osc.duration = _duration;
+            return osc;
+        }
+    };
 }
