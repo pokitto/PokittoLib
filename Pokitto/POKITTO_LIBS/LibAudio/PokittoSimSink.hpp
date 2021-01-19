274a26d87b228b78504924b11135de0cab4ec384
carbonacat <22575458+carbonacat@users.noreply.github.com>
2021-01-19 17:57:30 +0100

    FIXED popping on clang/macos.

--- Pokitto/POKITTO_LIBS/LibAudio/PokittoSimSink.hpp
+++ Pokitto/POKITTO_LIBS/LibAudio/PokittoSimSink.hpp
@@ -10,10 +10,10 @@ namespace Audio {
     }
 
     inline void mix(void *dst, const void* src, std::size_t count) {
-        char *buffer = static_cast<char*>(dst);
-        const char *tmp = static_cast<const char*>(src);
+        u8 *buffer = static_cast<u8*>(dst);
+        const u8 *tmp = static_cast<const u8*>(src);
         for(u32 i=0; i<count; ++i){
-            s32 v = s32(buffer[i]) + s32(tmp[i]) - 127;
+            s32 v = s32(buffer[i]) + s32(tmp[i]) - 128;
             if(v < 0) v = 0;
             else if(v > 0xFF) v = 0xFF;
             buffer[i] = v;
